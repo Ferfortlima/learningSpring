3ba768b7d0900d2e713c1eb8b338779bd8ba68c1
Fernando <fernando.fortlima@gmail.com>
2018-08-08 17:29:36 -0300

    added the services classes and implemented the repositories and erros handlers.

---
+++ BootEscola/src/main/java/escola/repository/DisciplinaRepository.java
@@ -0,0 +1,4 @@
+package escola.repository;
+
+public class DisciplinaRepository {
+}
---
+++ BootEscola/src/main/java/escola/util/ErrorPattern.java
@@ -0,0 +1,36 @@
+package escola.util;
+
+import java.io.Serializable;
+
+public class ErrorPattern implements Serializable {
+
+    private String code;
+    private String message;
+
+    public ErrorPattern() {
+    }
+
+    public ErrorPattern(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public ErrorPattern setCode(String code) {
+        this.code = code;
+        return this;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public ErrorPattern setMessage(String message) {
+        this.message = message;
+        return this;
+    }
+}
+
